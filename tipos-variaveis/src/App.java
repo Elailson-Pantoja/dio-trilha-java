bf1806b3a60be7bbe38526b04dbadd88ba23003b
Elailson Pantoja <elailson.pantoja13@gmail.com>
2025-04-01 22:28:52 -0300

    valor constantes

--- tipos-variaveis/src/App.java
+++ tipos-variaveis/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Testando");
+        final double  VALOR_PI = 3.14;
+
+        System.out.println(VALOR_PI);;
     }
 }
