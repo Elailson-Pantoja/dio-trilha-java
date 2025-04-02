5a1fe6512df7f61bb571f9899994943503adf707
Elailson Pantoja <elailson.pantoja13@gmail.com>
2025-04-01 23:03:12 -0300

    variavel boolean

--- tipos-variaveis/src/App.java
+++ tipos-variaveis/src/App.java
@@ -2,6 +2,10 @@ public class App {
     public static void main(String[] args) throws Exception {
         final double  VALOR_PI = 3.14;
 
-        System.out.println(VALOR_PI);;
+        System.out.println(VALOR_PI);
+
+        boolean variavel = true;
+        variavel = !variavel;
+        System.out.println(variavel);
     }
 }
