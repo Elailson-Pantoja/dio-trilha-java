7fe2432cc618236ce0a26afaa7b14e2e7f23d30c
Elailson Pantoja <elailson.pantoja13@gmail.com>
2025-04-04 22:43:41 -0300

    Documentando o código com tags

---
+++ tipos-variaveis/src/edu/elailson/tag/calculadora.java
@@ -0,0 +1,27 @@
+package edu.elailson.tag;
+
+/**
+ * <h1> Calculadora </h1>
+ * A Calcularora realiza operações matemáticas entre números inteiros
+ * <p>
+ * <b>Note: </b> Leia atentamente a documentação desta classe
+ * para desfrutar dos recursos oferecidos pelo autor
+ * 
+ * @author Elailson Pantoja
+ * @version 1.0
+ * @since 04/04/2025
+ * 
+ */
+public class calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado deste método é a soma dos dois números
+     * 
+     */
+
+    public int somar(int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+    }
+}
